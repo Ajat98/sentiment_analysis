ae67c6e4f706b91dc155a466bcfbfdd8b825db2b
Adam Harner <adamj.harner@gmail.com>
2020-06-23 14:36:24 -0400

    updated main sentiment analysis

--- twitter_sentiment_stream.py
+++ twitter_sentiment_stream.py
@@ -1,6 +1,21 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import json
+import sqlite3
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from textblob import TextBlob
+from unidecode import unidecode
+
+
+conn = sqlite3.connect('twitter.db')
+c = conn.cursor()
+def create_table():
+    #need to convert unix time (ms) into date or regular time
+    c.execute('CREATE TABLE IF NOT EXISTS sentiment (unix REAL, tweet TEXT, sentiment REAL)')
+    conn.commit()
+
+create_table()
 
 #Consumer key, consumer secret, access token, access secret
 #From aps.twitter.com
@@ -12,8 +27,27 @@ asecret = 'asecret here'
 class listener(StreamListener):
     
     def on_data(self, data):
-        print(data)
-        return True
+        try:
+            data = json.loads(data)
+            tweet = unidecode(data['text'])
+            time_ms = data['timestamp_ms']
+            analysis = TextBlob(line)
+            vs = analyzer.polarity_scores(tweet)
+            sentiment = vs['compound']
+            print(time_ms, tweet, sentiment)
+            c.execute("INSERT INTO sentiment (unix, tweet, sentiment) VALUES (?, ?, ?)", (time_ms, tweet, sentiment))
+            conn.commit()
+        except KeyError as e:
+            print(str(e))
+        return (True)
 
     def on_error(self, status):
         print(status)
+
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+#Streams things for the term 'car' can be changes
+twitterStream.filter(track=['car'])
